87c09e59e88a201265f3eaf69478408bce74b744
ryan.schuft <ryan.schuft@d2fe450a-162e-0410-afd2-a5a3f3c8e437>
2007-04-14 21:30:52 +0000

    Added support for overriding default lists of uncounted words and or default rules
    git-svn-id: http://inflection-js.googlecode.com/svn/trunk@18 d2fe450a-162e-0410-afd2-a5a3f3c8e437

--- inflection.js
+++ inflection.js
@@ -58,45 +58,59 @@ if(!String.prototype.pluralize)String.prototype.pluralize=function(plural)
   if(plural)str=plural;
   else
   {
-    var uncountable_words=[
-      'equipment','information','rice','money','species','series','fish',
-      'sheep','moose','deer'
-    ];
     var uncountable=false;
-    for(var x=0;!uncountable&&x<uncountable_words.length;x++)
-      uncountable=(uncountable_words[x].toLowerCase()==str.toLowerCase());
+    for(var x=0;!uncountable&&x<this._uncountable_words.length;x++)
+      uncountable=(this._uncountable_words[x]==str.toLowerCase());
     if(!uncountable) 
     {
-      var rules=[
-        [new RegExp('(m)an$','gi'),'$1en'],
-        [new RegExp('(pe)rson$','gi'),'$1ople'],
-        [new RegExp('(child)$','gi'),'$1ren'],
-        [new RegExp('(ax|test)is$','gi'),'$1es'],
-        [new RegExp('(octop|vir)us$','gi'),'$1i'],
-        [new RegExp('(alias|status)$','gi'),'$1es'],
-        [new RegExp('(bu)s$','gi'),'$1ses'],
-        [new RegExp('(buffal|tomat)o$','gi'),'$1oes'],
-        [new RegExp('([ti])um$','gi'),'$1a'],
-        [new RegExp('sis$','gi'),'ses'],
-        [new RegExp('(?:([^f])fe|([lr])f)$','gi'),'$1$2ves'],
-        [new RegExp('(hive)$','gi'),'$1s'],
-        [new RegExp('([^aeiouy]|qu)y$','gi'),'$1ies'],
-        [new RegExp('(x|ch|ss|sh)$','gi'),'$1es'],
-        [new RegExp('(matr|vert|ind)ix|ex$','gi'),'$1ices'],
-        [new RegExp('([m|l])ouse$','gi'),'$1ice'],
-        [new RegExp('^(ox)$','gi'),'$1en'],
-        [new RegExp('(quiz)$','gi'),'$1zes'],
-        [new RegExp('s$','gi'),'s'],
-        [new RegExp('$','gi'),'s']
-      ];
       var matched=false;
-      for(var x=0;!matched&&x<=rules.length;x++)
+      for(var x=0;!matched&&x<=this._plural_rules.length;x++)
       {
-        matched=str.match(rules[x][0]);
-        if(matched)str=str.replace(rules[x][0],rules[x][1]);
+        matched=str.match(this._plural_rules[x][0]);
+        if(matched)
+          str=str.replace(this._plural_rules[x][0],this._plural_rules[x][1]);
       }
     }
   }
   return str;
 };
+
+/*
+  This is a list of nouns that use the same form for both singular and plural.
+  This list should remain entirely in lower case to correctly match Strings.
+  You can override this list for all Strings or just one depending on if you
+  set the new values on prototype or on a given String instance.
+*/
+if(!String.prototype._uncountable_words)String.prototype._uncountable_words=[
+  'equipment','information','rice','money','species','series','fish','sheep',
+  'moose','deer'
+];
+
+/*
+  These rules translate from the singular form of a noun to its plural form.
+  You can override this list for all Strings or just one depending on if you
+  set the new values on prototype or on a given String instance.
+*/
+if(!String.prototype._plural_rules)String.prototype._plural_rules=[
+  [new RegExp('(m)an$','gi'),'$1en'],
+  [new RegExp('(pe)rson$','gi'),'$1ople'],
+  [new RegExp('(child)$','gi'),'$1ren'],
+  [new RegExp('(ax|test)is$','gi'),'$1es'],
+  [new RegExp('(octop|vir)us$','gi'),'$1i'],
+  [new RegExp('(alias|status)$','gi'),'$1es'],
+  [new RegExp('(bu)s$','gi'),'$1ses'],
+  [new RegExp('(buffal|tomat)o$','gi'),'$1oes'],
+  [new RegExp('([ti])um$','gi'),'$1a'],
+  [new RegExp('sis$','gi'),'ses'],
+  [new RegExp('(?:([^f])fe|([lr])f)$','gi'),'$1$2ves'],
+  [new RegExp('(hive)$','gi'),'$1s'],
+  [new RegExp('([^aeiouy]|qu)y$','gi'),'$1ies'],
+  [new RegExp('(x|ch|ss|sh)$','gi'),'$1es'],
+  [new RegExp('(matr|vert|ind)ix|ex$','gi'),'$1ices'],
+  [new RegExp('([m|l])ouse$','gi'),'$1ice'],
+  [new RegExp('^(ox)$','gi'),'$1en'],
+  [new RegExp('(quiz)$','gi'),'$1zes'],
+  [new RegExp('s$','gi'),'s'],
+  [new RegExp('$','gi'),'s']
+];
 
