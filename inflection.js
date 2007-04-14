bf0f1de30ca3876353eef77c04bc53020a806556
ryan.schuft <ryan.schuft@d2fe450a-162e-0410-afd2-a5a3f3c8e437>
2007-04-14 19:29:23 +0000

    Added MIT License
    git-svn-id: http://inflection-js.googlecode.com/svn/trunk@14 d2fe450a-162e-0410-afd2-a5a3f3c8e437

--- inflection.js
+++ inflection.js
@@ -1,9 +1,30 @@
 /*
+Copyright (c) 2007 Ryan Schuft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/*
   This code is based in part on the work done in Ruby to support
   infection as part of Ruby on Rails in the ActiveSupport's Inflector
   and Inflections classes.  It was initally ported to Javascript by
-  Ryan Schuft (ryan.schuft@gmail.com).  It is freely available without
-  limitation for reuse or extension by anyone for any usage scenario.
+  Ryan Schuft (ryan.schuft@gmail.com).
 
   The code is available at http://code.google.com/p/inflection-js/
 
