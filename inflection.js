28cb61a0a737ca22d3d35a11d98208959d66f249
ryan.schuft <ryan.schuft@d2fe450a-162e-0410-afd2-a5a3f3c8e437>
2007-04-14 22:58:14 +0000

    Added singularize support
    git-svn-id: http://inflection-js.googlecode.com/svn/trunk@19 d2fe450a-162e-0410-afd2-a5a3f3c8e437

--- inflection.js
+++ inflection.js
@@ -33,9 +33,14 @@ THE SOFTWARE.
     2. Call functions on any String object in Javascript
 
   Currently implemented functions:
+
     String.pluralize(plural) == String
       renders a singular English language noun into its plural form
       normal results can be overridden by passing in an alternative
+
+    String.singularize(singular) == String
+      renders a plural English language noun into its singular form
+      normal results can be overridden by passing in an alterative
 */
 
 /*
@@ -76,6 +81,45 @@ if(!String.prototype.pluralize)String.prototype.pluralize=function(plural)
 };
 
 /*
+  This function adds singularization support to every String object
+    Signature:
+      String.singularize(singular) == String
+    Arguments:
+      singular - String (optional) - overrides normal output with said String
+    Returns:
+      String - plural English language nouns are returned in singular form
+    Examples:
+      "people".singularize() == "person"
+      "octopi".singularize() == "octopus"
+      "Hats".singularize() == "Hat"
+      "guys".singularize("person") == "person"
+*/
+if(!String.prototype.singularize)
+  String.prototype.singularize=function(singular)
+  {
+    var str=this;
+    if(singular)str=singular;
+    else
+    {
+      var uncountable=false;
+      for(var x=0;!uncountable&&x<this._uncountable_words.length;x++)
+        uncountable=(this._uncountable_words[x]==str.toLowerCase());
+      if(!uncountable)
+      {
+        var matched=false;
+        for(var x=0;!matched&&x<=this._singular_rules.length;x++)
+        {
+          matched=str.match(this._singular_rules[x][0]);
+          if(matched)
+            str=str.replace(this._singular_rules[x][0],
+              this._singular_rules[x][1]);
+        }
+      }
+    }
+    return str;
+  };
+
+/*
   This is a list of nouns that use the same form for both singular and plural.
   This list should remain entirely in lower case to correctly match Strings.
   You can override this list for all Strings or just one depending on if you
@@ -83,7 +127,7 @@ if(!String.prototype.pluralize)String.prototype.pluralize=function(plural)
 */
 if(!String.prototype._uncountable_words)String.prototype._uncountable_words=[
   'equipment','information','rice','money','species','series','fish','sheep',
-  'moose','deer'
+  'moose','deer','news'
 ];
 
 /*
@@ -112,5 +156,39 @@ if(!String.prototype._plural_rules)String.prototype._plural_rules=[
   [new RegExp('(quiz)$','gi'),'$1zes'],
   [new RegExp('s$','gi'),'s'],
   [new RegExp('$','gi'),'s']
+];
+
+/*
+  These rules translate from the plural form of a noun to its singular form.
+  You can override this list for all Strings or just one depending on if you
+  set the new values on prototype or on a given String instance.
+*/
+if(!String.prototype._singular_rules)String.prototype._singular_rules=[
+  [new RegExp('(m)en$','gi'),'$1an'],
+  [new RegExp('(pe)ople$','gi'),'$1rson'],
+  [new RegExp('(child)ren$','gi'),'$1'],
+  [new RegExp('([ti])a$','gi'), '$1um'],
+  [new RegExp('((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$',
+    'gi'),'$1$2sis'],
+  [new RegExp('(hive)s$','gi'), '$1'],
+  [new RegExp('(tive)s$','gi'), '$1'],
+  [new RegExp('([lr])ves$','gi'), '$1f'],
+  [new RegExp('([^fo])ves$','gi'), '$1fe'],
+  [new RegExp('([^aeiouy]|qu)ies$','gi'), '$1y'],
+  [new RegExp('(s)eries$','gi'), '$1eries'],
+  [new RegExp('(m)ovies$','gi'), '$1ovie'],
+  [new RegExp('(x|ch|ss|sh)es$','gi'), '$1'],
+  [new RegExp('([m|l])ice$','gi'), '$1ouse'],
+  [new RegExp('(bus)es$','gi'), '$1'],
+  [new RegExp('(o)es$','gi'), '$1'],
+  [new RegExp('(shoe)s$','gi'), '$1'],
+  [new RegExp('(cris|ax|test)es$','gi'), '$1is'],
+  [new RegExp('(octop|vir)i$','gi'), '$1us'],
+  [new RegExp('(alias|status)es$','gi'), '$1'],
+  [new RegExp('^(ox)en','gi'), '$1'],
+  [new RegExp('(vert|ind)ices$','gi'), '$1ex'],
+  [new RegExp('(matr)ices$','gi'), '$1ix'],
+  [new RegExp('(quiz)zes$','gi'), '$1'],
+  [new RegExp('s$','gi'), '']
 ];
 
